2f40c780c210ab577926ef96995bd7db6722d11c
Dada <dai_fs6gwws4386@outlook.jp>
2019-06-25 23:41:44 +0900

    GET用pythonアプリ試作

---
+++ docker/flask_get_app/hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, abort, make_response
+
+fapi = Flask(__name__)
+
+@fapi.route('/', methods=['GET'])
+def get():
+    res = { "message": "hello world" }
+    return make_response(jsonify(res))
+
+if __name__ == '__main__':
+    fapi.run(host='0.0.0.0', port=3000)
