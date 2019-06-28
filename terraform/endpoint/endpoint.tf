e66f739d0575c05c4e2774a67d5437bfcca74cf1
Dada <dai_fs6gwws4386@outlook.jp>
2019-06-29 00:19:50 +0900

    deploymentのlayer名などを修正

--- terraform/endpoint/endpoint.tf
+++ terraform/endpoint/endpoint.tf
@@ -1,7 +1,7 @@
 variable "openapi_path" {}
 
 resource "google_endpoints_service" "openapi_service" {
-  service_name   = "api-master.endpoints.teraform-244604.cloud.goog"
+  service_name   = "hello-api.endpoints.teraform-244604.cloud.goog"
   project        = "teraform-244604"
   openapi_config = "${file(var.openapi_path)}"
 }
--- terraform/k8s/pods.tf
+++ terraform/k8s/pods.tf
@@ -1,33 +1,33 @@
-resource "kubernetes_replication_controller" "api-master" {
+resource "kubernetes_replication_controller" "hello-api" {
   metadata {
-    name = "api-master"
+    name = "hello-api"
     labels = {
-      app  = "api-app"
+      app  = "endpoints-app"
     }
   }
 
   spec {
     replicas = 1
     selector = {
-      app = "api-app"
+      app = "endpoints-app"
     }
 
     template {
       container {
-        name = "endpoints-master"
+        name = "esp"
         image = "gcr.io/endpoints-release/endpoints-runtime:1"
         args = [
-          "-p=8081",
-          "-a=0.0.0.0:3000",
-          "-s=api-master.endpoints.teraform-244604.cloud.goog",
-          "-R=managed"
+          "--http_port=8081",
+          "--backend=0.0.0.0:3000",
+          "--service=hello-api.endpoints.teraform-244604.cloud.goog",
+          "--rollout_strategy=managed"
         ]
         port {
           container_port = 8081
         }
       }
       container {
-        name = "api-master"
+        name = "hello-api"
         image = "asia.gcr.io/teraform-244604/get-hello:latest"
         port {
           container_port = 3000
