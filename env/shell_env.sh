7a13be869687d9c9c277416da580c22663f25a51
Dada <dai_fs6gwws4386@outlook.jp>
2019-06-29 17:25:38 +0900

    k8s ingressのテスト

--- env/shell_env.sh
+++ env/shell_env.sh
@@ -5,3 +5,7 @@ export TERRAFORM_SA_EMAIL=$(gcloud iam service-accounts list \
     --filter="displayName:$TERRAFORM_SA" \
     --format='value(email)')
 export GOOGLE_APPLICATION_CREDENTIALS=/home/dada/.gcp/terraform-account.json
+
+gcloud config set compute/zone asia-northeast1-a
+gcloud config set project teraform-244604
+gcloud container clusters get-credentials primary-microservice-test
