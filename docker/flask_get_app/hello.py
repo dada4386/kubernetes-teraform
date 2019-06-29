7af42e35fb58b9d1a499af677f6f1d1e7ae3b403
Dada <dai_fs6gwws4386@outlook.jp>
2019-06-29 17:26:37 +0900

    healthcheckの導入

--- docker/flask_get_app/hello.py
+++ docker/flask_get_app/hello.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python3
 from flask import Flask, jsonify, abort, make_response
 
 fapi = Flask(__name__)
 
 @fapi.route('/hello', methods=['GET'])
-def get():
+def hello():
     res = { "message": "hello world" }
     return make_response(jsonify(res))
 
+@fapi.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    res = { "status": "ok" }
+    return make_response(jsonify(res))
+
 if __name__ == '__main__':
-    fapi.run(host='0.0.0.0', port=3000)
+    fapi.run(host='0.0.0.0', port=3000, threaded=True)
