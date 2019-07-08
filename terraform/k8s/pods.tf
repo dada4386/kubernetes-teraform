8896b8c4fcf28e43589c87a81b2716185b682d70
Dada <dai_fs6gwws4386@outlook.jp>
2019-07-08 23:32:57 +0900

    RCからDeploymentに変更

--- terraform/k8s/pods.tf
+++ terraform/k8s/pods.tf
@@ -1,4 +1,4 @@
-resource "kubernetes_replication_controller" "hello-api" {
+resource "kubernetes_deployment" "hello-api" {
   metadata {
     name = "hello-api"
     labels = {
@@ -7,46 +7,56 @@ resource "kubernetes_replication_controller" "hello-api" {
   }
 
   spec {
-    replicas = 1
-    selector = {
-      app = "endpoints-app"
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "endpoints-app"
+      }
     }
 
     template {
-      container {
-        name = "esp"
-        image = "gcr.io/endpoints-release/endpoints-runtime:1"
-        args = [
-          "--http_port=8081",
-          "--backend=0.0.0.0:3000",
-          "--service=hello-api.endpoints.teraform-244604.cloud.goog",
-          "--rollout_strategy=managed"
-        ]
-        port {
-          container_port = 8081
-        }
-        liveness_probe {
-          http_get {
-            path = "/healthcheck"
-            port = "8081"
-          }
-          initial_delay_seconds = 3
-          period_seconds = 3
-        }
-        readiness_probe {
-          http_get {
-            path = "/healthcheck"
-            port = "8081"
-          }
-          initial_delay_seconds = 3
-          period_seconds = 3
+      metadata {
+        labels = {
+          app = "endpoints-app"
         }
       }
-      container {
-        name = "hello-api"
-        image = "asia.gcr.io/teraform-244604/get-hello:${var.container_tag}"
-        port {
-          container_port = 3000
+
+      spec {
+        container {
+          name = "esp"
+          image = "gcr.io/endpoints-release/endpoints-runtime:1"
+          args = [
+            "--http_port=8081",
+            "--backend=0.0.0.0:3000",
+            "--service=hello-api.endpoints.teraform-244604.cloud.goog",
+            "--rollout_strategy=managed"
+          ]
+          port {
+            container_port = 8081
+          }
+          liveness_probe {
+            http_get {
+              path = "/healthcheck"
+              port = "8081"
+            }
+            initial_delay_seconds = 3
+            period_seconds = 3
+          }
+          readiness_probe {
+            http_get {
+              path = "/healthcheck"
+              port = "8081"
+            }
+            initial_delay_seconds = 3
+            period_seconds = 3
+          }
+        }
+        container {
+          name = "hello-api"
+          image = "asia.gcr.io/teraform-244604/get-hello:latest"
+          port {
+            container_port = 3000
+          }
         }
       }
     }
