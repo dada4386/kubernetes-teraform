ab0ed28e17df76a13f1112259c3d12058f79e4e8
Dada <dai_fs6gwws4386@outlook.jp>
2019-07-08 23:53:36 +0900

    endpointsをマージ

--- terraform/k8s/endpoints_api.tf
+++ terraform/k8s/endpoints_api.tf
@@ -1,3 +1,24 @@
+resource "kubernetes_service" "endpoints" {
+  metadata {
+    name = "endpoints-service"
+    labels = {
+      apps = "endpoints-service"
+    }
+  }
+
+  spec {
+    selector = {
+      app  = "endpoints-app"
+    }
+    port {
+      protocol    = "TCP"
+      port        = 8081
+      target_port = 8081
+    }
+    type = "NodePort"
+  }
+}
+
 resource "kubernetes_deployment" "hello-api" {
   metadata {
     name = "hello-api"
